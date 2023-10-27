bf5514312ab0b5fe7cee0da8947931b03295a4c2
enigma9119 <enigma9119@users.noreply.github.com>
2023-10-26 21:11:43 -0700

    Add base game logic & update the display
    Add game logic to play a round of the game (without checking winning
    condition). Connect the game controller to the display, to update the
    cells and current player div.

--- script.js
+++ script.js
@@ -1,11 +1,91 @@
-function Player(name) {
-  return {};
+function Player(id, sign) {
+  return { id, sign };
+}
+
+function Cell() {
+  let currentValue = "";
+
+  const getValue = () => currentValue;
+  const setValue = value => (currentValue = value);
+
+  return { getValue, setValue };
 }
 
 const gameBoard = (function () {
-  let board = [];
+  const [rows, cols] = [3, 3];
+  let board = Array.from({ length: rows }, () => Array(cols).fill(Cell()));
 
-  return { board };
+  const getBoard = () => board;
+  const getMark = (row, col) => board[row][col].getValue();
+
+  const addMark = (row, col, sign) => {
+    const cell = board[row][col];
+    cell.setValue(sign);
+  };
+
+  return { getBoard, getMark, addMark };
 })();
 
-const game = function () {};
+const gameController = (function () {
+  const [player1, player2] = [Player(1, "X"), Player(2, "O")];
+  let currentPlayer = player1;
+
+  const playRound = (row, col) => {
+    gameBoard.addMark(row, col, currentPlayer.sign);
+
+    // Check winning condition
+
+    switchPlayer();
+  };
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    displayController.updateCurrentPlayer(currentPlayer);
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  return { playRound, getCurrentPlayer };
+})();
+
+const displayController = (function () {
+  const board = document.querySelector(".game-container");
+  const currPlayerDiv = document.querySelector(".current-player");
+
+  const handleBoardClick = e => {
+    // Return if empty space between cells was clicked or if the cell is already filled
+    if (e.target.dataset.row === undefined || e.target.textContent) return;
+
+    updateGameCell(e.target, gameController.getCurrentPlayer());
+    gameController.playRound(e.target.dataset.row, e.target.dataset.col);
+  };
+
+  board.addEventListener("click", handleBoardClick);
+
+  const updateGameCell = (cellButton, player) => {
+    cellButton.textContent = player.sign;
+  };
+
+  const updateCurrentPlayer = player => {
+    const currPlayer =
+      player.id === 1
+        ? currPlayerDiv.firstElementChild
+        : currPlayerDiv.lastElementChild;
+    const otherPlayer =
+      currPlayer === currPlayerDiv.firstElementChild
+        ? currPlayerDiv.lastElementChild
+        : currPlayerDiv.firstElementChild;
+
+    const bodyStyle = getComputedStyle(document.body);
+    currPlayer.style.backgroundColor =
+      bodyStyle.getPropertyValue("--accent-color");
+    currPlayer.style.opacity = 1;
+
+    otherPlayer.style.backgroundColor = "gainsboro";
+    otherPlayer.style.opacity = 0.5;
+  };
+
+  updateCurrentPlayer(gameController.getCurrentPlayer());
+
+  return { updateCurrentPlayer };
+})();
